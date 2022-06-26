ed8372efe66da2a0c67c1e872036148bc11ac31e
esthieuk-nayeem <71044260+esthieuk-nayeem@users.noreply.github.com>
2022-06-26 16:01:35 +0600

    first commit

---
+++ api/views.py
@@ -0,0 +1,26 @@
+from typing import ItemsView
+from django.shortcuts import render
+from urllib import response
+
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+
+from base.models import Item
+from . serializers import ItemSerializer
+
+# Create your views here.
+
+@api_view(['GET'])
+def getData(request):
+    items = Item.objects.all()
+    serializer = ItemSerializer(items, many=True)
+
+    return Response(serializer.data)
+
+@api_view(['POST'])
+def addItem(request):
+    serializer = ItemSerializer(data= request.data)
+    if serializer.is_valid():
+        serializer.save()
+
+    return Response(serializer.data)
